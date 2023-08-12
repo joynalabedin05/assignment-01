f378b4f5a4e24dd7bd7ab9351c6b19c3cff800aa
joynalabedin05 <joynal05101993@gmail.com>
2023-08-12 10:30:50 +0600

    first

---
+++ app.js
@@ -0,0 +1,11 @@
+function find_max(nums) {
+     let max_num = Number.NEGATIVE_INFINITY; // smaller than all other numbers
+     for (let num of nums) {
+     if (num > max_num) {
+     // (Fill in the missing line here)
+     }
+     }
+     return max_num;
+     }
+    const result =  find_max([1,2,33,3]);
+    console.log(result)
